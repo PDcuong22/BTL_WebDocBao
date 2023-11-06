b0781ad0285d1f71906851d47d3cd0fbaa08ac28
Uremine02 <phammanhcuong1808@gmail.com>
2023-11-06 22:32:47 +0700

    Home+detail Layout

--- src/components/Layouts/DetailLayout/index.js
+++ src/components/Layouts/DetailLayout/index.js
@@ -1,9 +1,19 @@
+import classNames from "classnames/bind";
+import styles from "./DetailLayout.module.scss";
+import Header from "../../common/Header";
+
+const cx = classNames.bind(styles);
 function DetailLayout({ content }) {
-    return (
-      <div className="wrapper">
-        <div className="content">{content}</div>
+  return (
+    <div className={cx("wrapper")}>
+      <Header />
+      <div className={cx("content")}>
+        <div className={cx("partLeft")}>{content}</div>
+        <div className={cx("partRight")}></div>
       </div>
-    );
-  }
-  
-  export default DetailLayout;
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default DetailLayout;
--- src/components/Layouts/HomeLayout/index.js
+++ src/components/Layouts/HomeLayout/index.js
@@ -1,9 +1,21 @@
+import Footer from "../../common/Footer";
+import Header from "../../common/Header";
+
+import classNames from "classnames/bind";
+import styles from "./HomeLayout.module.scss";
+
+const cx = classNames.bind(styles);
 function HomeLayout({ content }) {
-    return (
-      <div className="wrapper">
-        <div className="content">{content}</div>
+  return (
+    <div className={cx("wrapper")}>
+      <Header />
+      <div className={cx("content")}>
+        <div className={cx("partLeft")}>{content}</div>
+        <div className={cx("partRight")}></div>
       </div>
-    );
-  }
-  
-  export default HomeLayout;
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default HomeLayout;
