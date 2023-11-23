fe07b63bbc667256c890d6cafe022ac23fbc3404
Uremine02 <phammanhcuong1808@gmail.com>
2023-11-23 13:18:16 +0700

    update category2

--- src/components/CategoryPart2/index.jsx
+++ src/components/CategoryPart2/index.jsx
@@ -1,78 +1,43 @@
 import classNames from "classnames/bind";
 import styles from "./CategoryPart2.module.scss";
-import SquareIcon from "@mui/icons-material/Square";
-import { subCateogryApi } from "../../services/subCateogry-api";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { articleApi } from "../../services/article-api";
 
 const cx = classNames.bind(styles);
 
-function CategoryPart2({ categoryId, categoryName, subId }) {
-  const [subCategories, setSubCategories] = useState([]);
-  const [articleLevel1, setArticleLevel1] = useState([]);
-  const [articleLevel2, setArticleLevel2] = useState([]);
-  useEffect(() => {
-    getSubCategories();
-  }, []);
-
-  useEffect(() => {
-    getArticles();
-  }, []);
-
-  const getSubCategories = async () => {
-    let res = await subCateogryApi.getSubCategories(categoryId);
-    setSubCategories(res);
-  };
-
-  const getArticles = async () => {
-    let res = await articleApi.getArticleBySubCategory(subId);
-    setArticleLevel1(res?.filter((item) => item.level === 1));
-    setArticleLevel2(res?.filter((item) => item.level === 2));
-  };
-
+function CategoryPart2() {
   return (
     <div className={cx("wrapper")}>
       <div className={cx("header")}>
-        <div className={cx("header-item")}>{categoryName}</div>
-        {subCategories.map((item, index) => (
-          <Link
-            to={`/subcategory/${item._id}`}
-            style={{ textDecoration: "none" }}
-          >
-            <div className={cx("header-item2")}>{item.subCategoryName}</div>
-          </Link>
-        ))}
+        <div className={cx("header-item")}>Giáo dục du học</div>
+        <div className={cx("header-item2")}>Dạy con</div>
+        <div className={cx("header-item2")}>Trắc nghiệm</div>
+        <div className={cx("header-item2")}>Tuyển sinh ĐH - CĐ</div>
+        <div className={cx("header-item2")}>Tuyển sinh lớp 10</div>
       </div>
-      {articleLevel1.slice(0, 1).map((item, i) => (
-        <>
-          <p className={cx("tittle")}>{item.header}</p>
-          <div className={cx("news")}>
-            <img src={item.image} className={cx("image")} />
-            <p className={cx("content")}>
-              “Để phòng chống rủi ro, thí sinh nên đặt một số nguyện vọng và san
-              đều ở những nhóm trường có mức độ cạnh tranh khác nhau, đừng dồn
-              tất cả nguyện vọng vào các trường top cao”- PGS.TS Nguyễn Thu
-              Thủy,...
-            </p>
-          </div>
-        </>
-      ))}
-      <div className={cx("news1")}>
-        {articleLevel1.slice(2).map((item) => (
-          <div className={cx("item")}>
-            <SquareIcon sx={{ fontSize: 8, color: "var(--primary-color)" }} />
-            <a>{item.header}</a>
-          </div>
-        ))}
+      <p className={cx("tittle")}>
+      Vụ trưởng Vụ Giáo dục Đại học: Đừng dồn tất cả nguyện vọng vào các trường top cao
+      </p>
+      <div className={cx("news")}>
+        <img src="https://icdn.24h.com.vn/upload/3-2023/images/2023-07-21//medium/adt1689907972-k2-373.jpg" 
+        width="120"
+        height="90"
+        />
+        <p className={cx("content")}>
+        “Để phòng chống rủi ro, thí sinh nên đặt một số nguyện vọng và san đều ở những nhóm trường có mức độ cạnh tranh khác nhau, đừng dồn tất cả nguyện vọng vào các trường top cao”- PGS.TS Nguyễn Thu Thủy,...
+        </p>
       </div>
       <div className={cx("news1")}>
-        {articleLevel2.slice(0, 3).map((item) => (
-          <div className={cx("item")}>
-            <SquareIcon sx={{ fontSize: 8, color: "var(--primary-color)" }} />
-            <a>{item.header}</a>
-          </div>
-        ))}
+        <div className={cx("item")}>
+          <a>Ngành 'hot' điểm chuẩn dự kiến không dưới 27 điểm</a>
+        </div>
+        <div className={cx("item")}>
+          <a>34 tuổi thi đại học 15 lần, người đàn ông đã chịu nhập học</a>
+        </div>
+        <div className={cx("item")}>
+          <a>Điểm chuẩn Trường ĐH Kinh tế Quốc dân dự kiến như thế nào?</a>
+        </div>
+        <div className={cx("item")}>
+          <a>Được hỗ trợ 100% học phí, học sinh Đà Nẵng tiết kiệm được bao nhiêu mỗi tháng?</a>
+        </div>
       </div>
     </div>
   );
