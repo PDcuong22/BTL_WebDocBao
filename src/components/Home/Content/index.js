a5355d29a4c38b21d9aac79c25adbc5fd0a12e6e
Uremine02 <phammanhcuong1808@gmail.com>
2023-12-01 20:22:08 +0700

    add content

--- src/components/Home/Content/index.js
+++ src/components/Home/Content/index.js
@@ -3,37 +3,37 @@ import styles from "./Content.module.scss";
 import CategoryPart from "../../CategoryPart";
 import CategoryPart2 from "../../CategoryPart2";
 import { articleApi } from "../../../services/article-api";
-import { useEffect } from "react";
 import { useEffect, useState } from "react";
 const cx = classNames.bind(styles);
 
 function Content() {
-  useEffect(() => {
-    getArticles();
-  }, []);
-  const getArticles = async () => {
-    let res = await articleApi.getArticles();
-    console.log("List: ", res);
-  };
+  // useEffect(() => {
+  //   getArticles();
+  // }, []);
+
+  // const getArticles = async () => {
+  //   let res = await articleApi.getArticles();
+  //   console.log("List: ", res);
+  // };
+
   return (
     <div className={cx("wrapper")}>
       <div className={cx("top")}>
-        <CategoryPart />
-        <CategoryPart />
-        <CategoryPart />
-        <CategoryPart />
-        <CategoryPart />
-        <CategoryPart />
+        <CategoryPart subCategoryId="64bba1ecf3d0c05481f80a6c" />
+        <CategoryPart subCategoryId="64bba231f3d0c05481f80a75" />
+        <CategoryPart subCategoryId="64c396481d8357c80824505e" />
+        <CategoryPart subCategoryId="64bba147f3d0c05481f80a55" />
+        <CategoryPart subCategoryId="64c382d72833a8bce95440f7" />
+        <CategoryPart subCategoryId="64bb9e3bf3d0c05481f80a11" />
       </div>
       <div className={cx("bottom")}>
-        <CategoryPart2 />
-        <CategoryPart2 />
-        <CategoryPart2 />
-        <CategoryPart2 />
-        <CategoryPart2 />
-        <CategoryPart2 />
+        <CategoryPart2
+          subName="Giáo dục"
+          categoryId="64a79fedc573829b5238bd99"
+        />
       </div>
     </div>
   );
 }
+
 export default Content;
