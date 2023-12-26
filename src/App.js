4d2b48ed812aa9c1bdecfc8863bc7c1198c29349
UreMine02 <117183874+UreMine02@users.noreply.github.com>
2023-12-26 08:31:40 +0700

    add route

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import Detail1 from "./pages/Detail1";
 import SubCategory from "./pages/SubCategory";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MyEditor from "./pages/MyEditor";
+import AdminPage from "./pages/Admin/Adminpage.jsx";
+import CreateArticlePage from "./pages/Admin/CreateArticlePage.jsx";
+import EditArticlePage from "./pages/Admin/EditArticlePage.jsx";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
-  return (
+  return (<>
+    <ToastContainer position="top-center" />
     <Router>
       <Routes>
         <Route path="/insert" element={<MyEditor />} />
@@ -13,8 +20,12 @@ function App() {
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="/detail/test/:id" element={<Detail1 />} />
         <Route path="/subCategory/:subcategoryId" element={<SubCategory />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/admin/article/create" element={<CreateArticlePage />} />
+        <Route path="/admin/article/edit/:articleId" element={<EditArticlePage />} />
       </Routes>
     </Router>
+  </>
   );
 }
 
